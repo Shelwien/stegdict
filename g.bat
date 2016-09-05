5318a97864099d91a6e9d40f878292ebf54420c9
Shelwien <shelwien.san@gmail.com>
2016-09-05 06:28:04 +0300

    commit

--- g.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-rem "C:\MinGW700x\bin\g++.exe" -O9 -s -ILib -fwhole-program -fno-exceptions -fno-rtti -static stegdict.cpp -o stegdict
-
-"C:\MinGW700\bin\g++.exe" -m32 -O9 -s -ILib -fwhole-program -fno-exceptions -fno-rtti -static stegdict.cpp -o stegdict
